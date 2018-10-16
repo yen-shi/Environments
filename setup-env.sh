e75050126a5b17e6507c374b03c3ca26dc7b05a2
yen-shi <danny8508021@gmail.com>
2018-10-17 01:26:20 +0800

    Complete Vim setup

--- setup-env.sh
+++ setup-env.sh
@@ -3,8 +3,21 @@
 # get vim configuration
 cp ./configs/.vimrc ${HOME}
 
-# install vim packages
+# clone Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# install all packages listed in vimrc
+vim +PluginInstall +qall
 
 # get git configuration
+cp ./configs/.gitconfig ${HOME}
 
 # ssl key gen
+# manually copy from original computer
+echo "Don't forget to copy ssh config (or keys) from original computer."
+
+# change shell to zsh
+apt install -y zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+chsh -s /bin/zsh
+echo "You need to reboot to change the default shell."
