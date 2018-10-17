1005df8df77ba7e6ff6fdf2843926834a8ea7414
yen-shi <danny8508021@gmail.com>
2018-10-17 09:37:45 +0800

    Add JAVA installation

--- setup-env.sh
+++ setup-env.sh
@@ -23,4 +23,7 @@ echo "==========================================================================
 apt install -y zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 chsh -s /bin/zsh
-echo "You need to reboot to change the default shell."
+echo "You need to re-login to change the default shell."
+
+cp ./configs/.custom-shrc ${HOME}
+echo "source \${HOME}/.custom-shrc" >> ${HOME}/.zshrc
